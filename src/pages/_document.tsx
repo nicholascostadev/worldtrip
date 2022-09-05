73dafb1b5c411560296ce000ef3a24c2daab0a97
Nicholas Costa <nicholascostadev@gmail.com>
2022-09-05 12:45:29 -0300

    :fire: Remove font import from _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,7 @@ import { ColorModeScript } from '@chakra-ui/react'
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head />
       <body>
         <ColorModeScript initialColorMode="dark" />
         <Main />
