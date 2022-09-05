f6d47627991895091a6a2cefc3b42459bd3b020e
Nicholas Costa <nicholascostadev@gmail.com>
2022-09-05 10:24:58 -0300

    :recycle: refactor: Rearrange files

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>WorldTrip</title>
+      </Head>
+
+      <main>
+        <div>
+          <h1>WorldTrip</h1>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home
