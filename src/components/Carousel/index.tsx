f854647c225ec34498fdc890574a8322cfe24542
Nicholas Costa <nicholascostadev@gmail.com>
2022-09-05 22:26:11 -0300

    :recycle: refactor: Loop carousel through options instead of manual apply

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,106 +1,91 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
-import europeImage from '../../assets/europe.png'
+import AfricaImage from '../../assets/Africa.jpg'
+import AsiaImage from '../../assets/Asia.jpg'
+import EuropeImage from '../../assets/europe.png'
 import NAImage from '../../assets/NA.jpg'
 import OceaniaImage from '../../assets/Oceania.jpg'
 import SAImage from '../../assets/SAImage.jpg'
-import AfricaImage from '../../assets/Africa.jpg'
-import AsiaImage from '../../assets/Asia.jpg'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 
 import { Box, Heading, Text, VStack } from '@chakra-ui/react'
 import Image from 'next/image'
-import { Navigation, Pagination } from 'swiper'
 import Link from 'next/link'
+import { Navigation, Pagination } from 'swiper'
+
+const carousels = [
+  {
+    image: NAImage,
+    heading: 'América do Norte',
+    text: 'O Continente da tecnologia',
+  },
+  {
+    image: SAImage,
+    heading: 'América do Sul',
+    text: 'O Continente da putaria',
+  },
+  {
+    image: OceaniaImage,
+    heading: 'Oceania',
+    text: 'O novíssimo continente.',
+  },
+  {
+    image: AfricaImage,
+    heading: 'África',
+    text: 'O continente da pluralidade étnica e cultural.',
+  },
+  {
+    image: EuropeImage,
+    heading: 'Europa',
+    text: 'O continente mais antigo.',
+  },
+  {
+    image: AsiaImage,
+    heading: 'Ásia',
+    text: 'O Continente dos xinglinglinglongpingpong',
+  },
+] as const
 
 export const Carousel = () => {
   return (
-    <Link href="/continent/" passHref>
-      <Box as="a" display="block" w="1240px" h="450px" mt="52px" mb="40px">
-        <Swiper
-          navigation={true}
-          modules={[Navigation, Pagination]}
-          spaceBetween={50}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-          className="swiper"
-        >
-          <SwiperSlide className="swiper-slide">
-            <Image src={europeImage} alt="" layout="fill" />
+    <Box display="block" w="1240px" h="450px" mt="52px" mb="40px">
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        className="swiper"
+      >
+        {carousels.map((item) => (
+          <SwiperSlide key={item.heading} className="swiper-slide">
+            <Image src={item.image} alt="" layout="fill" />
+
             <VStack color="white">
-              <Heading zIndex="base">Europa</Heading>
-              <Text zIndex="base">O continente mais antigo.</Text>
+              <Link
+                key={item.heading}
+                href={`/continent/${item.heading}`}
+                passHref
+              >
+                <Heading as="a" zIndex="base">
+                  {item.heading}
+                </Heading>
+              </Link>
+
+              <Link
+                key={item.heading}
+                href={`/continent/${item.heading}`}
+                passHref
+              >
+                <Text as="a" zIndex="base">
+                  {item.text}
+                </Text>
+              </Link>
             </VStack>
           </SwiperSlide>
-          <SwiperSlide className="swiper-slide">
-            <Image
-              src={NAImage}
-              alt=""
-              layout="fill"
-              style={{ opacity: 0.7 }}
-            />
-            <VStack color="white">
-              <Heading zIndex="base">América do Norte</Heading>
-              <Text zIndex="base">O continente da Tecnologia.</Text>
-            </VStack>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide">
-            <Image
-              src={AfricaImage}
-              alt=""
-              layout="fill"
-              style={{ opacity: 0.7 }}
-            />
-            <VStack color="white">
-              <Heading zIndex="base">África</Heading>
-              {/* <Text zIndex="base">
-              O continente da pluralidade étnica e cultural.
-            </Text> */}
-              <Text zIndex="base">O novíssimo continente.</Text>
-            </VStack>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide">
-            <Image
-              src={OceaniaImage}
-              alt=""
-              layout="fill"
-              style={{ opacity: 0.7 }}
-            />
-            <VStack color="white">
-              <Heading zIndex="base">Oceania</Heading>
-              {/* <Text zIndex="base">O novíssimo continente.</Text> */}
-              <Text zIndex="base">O novíssimo continente.</Text>
-            </VStack>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide">
-            <Image
-              src={SAImage}
-              alt=""
-              layout="fill"
-              style={{ opacity: 0.7 }}
-            />
-            <VStack color="white">
-              <Heading zIndex="base">América do Sul</Heading>
-              {/* <Text zIndex="base">O continente .</Text> */}
-              <Text zIndex="base">O novíssimo continente.</Text>
-            </VStack>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide">
-            <Image
-              src={AsiaImage}
-              alt=""
-              layout="fill"
-              style={{ opacity: 0.7 }}
-            />
-            <VStack color="white">
-              <Heading zIndex="base">Europa</Heading>
-              {/* <Text zIndex="base">O continente mais antigo.</Text> */}
-              <Text zIndex="base">O novíssimo continente.</Text>
-            </VStack>
-          </SwiperSlide>
-        </Swiper>
-      </Box>
-    </Link>
+        ))}
+      </Swiper>
+    </Box>
   )
 }
